525a66012b3ccfbea504223588527a5e89875a76
Tito Yslas <roberto.yslas@gmail.com>
2017-12-12 12:37:49 -0700

    implemented the backdrop component - placed within the modal component - enabled click functionality to un-show the modal component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,6 +77,10 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: true});
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
   render () {
     const disabledInfo = {
       ...this.state.ingredients
@@ -87,7 +91,9 @@ class BurgerBuilder extends Component {
     // {salad: true, bacon: true, cheese: false, meat: false}
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />

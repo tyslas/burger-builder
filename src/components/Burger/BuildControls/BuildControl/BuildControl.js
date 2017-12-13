d81ef076af54dcb17bfaa6ffc14c8ad7649fda9c
Tito Yslas <roberto.yslas@gmail.com>
2017-12-13 12:52:34 -0700

    fixed issue with more/less buttons not sitting next to each other, added toolbar component with some basic JSX and CSS

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,14 +5,12 @@ import classes from './BuildControl.css'
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <span className={classes.Inline}>
       <button
       className={classes.Less}
       onClick={props.removed}
       disabled={props.disabled}>Less</button>
       <button
         className={classes.More} onClick={props.added}>More</button>
-    </span>
   </div>
 );
 

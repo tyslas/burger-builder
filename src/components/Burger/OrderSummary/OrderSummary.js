96064ff1ba07f56c29a1c86f1f8052e534c5bae5
Tito Yslas <roberto.yslas@gmail.com>
2017-12-13 11:42:48 -0700

    implemented the button component within the OrderSummary component with btnType and onClick listeners passed to the button via props from BurgerBuilder->OrderSummary->Button

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -20,8 +21,12 @@ const orderSummary = (props) => {
         {ingredientSummary}
       </ul>
       <p>Continue to Checkout?</p>
-      <button>CANCEL</button>
-      <button>CONTINUE</button>
+      <Button
+        btnType="Danger"
+        clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button
+        btnType="Success"
+        clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   )
 };

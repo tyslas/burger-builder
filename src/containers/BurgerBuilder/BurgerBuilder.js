d6499aeac580813a7ff955e382084745d55e5499
Tito Yslas <roberto.yslas@gmail.com>
2017-12-13 12:13:19 -0700

    added price of the burger to the OrderSummary component by passing down the totalPrice from the state of the BurgerBuilder component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -100,8 +100,9 @@ class BurgerBuilder extends Component {
           modalClosed={this.purchaseCancelHandler}>
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler}/>
+            purchaseContinued={this.purchaseContinueHandler} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls

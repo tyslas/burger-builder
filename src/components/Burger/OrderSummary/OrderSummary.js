817959476e814e44050e0b8d4bfca2c876288288
Tito Yslas <roberto.yslas@gmail.com>
2017-12-17 15:56:51 -0700

    improved performance of the app by restricting the OrderSummary component from updating without first rendering the Modal component. Used the shouldComponentUpdate lifecycle hook to verify the changed props

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>
-      )
-    })
+class OrderSummary extends Component {
+  //not neccesary for this to be a class component - this could be a functional component
+  // componentWillUpdate() {
+  //   console.log('[OrderSummery] WillUpdate');
+  // }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>a delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button
-        btnType="Danger"
-        clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button
-        btnType="Success"
-        clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  )
-};
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (
+          <li key={igKey}>
+            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+          </li>
+        )
+      })
 
-export default orderSummary;
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>a delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button
+          btnType="Danger"
+          clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button
+          btnType="Success"
+          clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;

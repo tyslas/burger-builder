e393c3d24ac6e9a1847faf599966ce84ee19befd
Tito Yslas <roberto.yslas@gmail.com>
2018-01-05 08:42:24 -0700

    a spinner will display while waiting for a post request to send back a response

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,7 +7,7 @@ import Backdrop from '../Backdrop/Backdrop';
 class Modal extends Component {
   //prevents the OrderSummary component from uneccessarily updating (i.e. when the component is not visible, it doesn't update)
   shouldComponentUpdate(nextProps, nextState) {
-      return nextProps.show !== this.props.show;
+      return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
 
   componentWillUpdate() {
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import axios from '../../axios-orders';
 
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -24,7 +25,8 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchaseable: false,
-    purchasing: false
+    purchasing: false,
+    loading: false
   }
 
   updatePurchaseState (ingredients) {
@@ -84,6 +86,7 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
+    this.setState( {loading: true} );
     /* in a production app you would calculate the totalPrice from the server side to ensure that the user couldn't manipulate the price */
     const order = {
       ingredients: this.state.ingredients,
@@ -100,10 +103,15 @@ class BurgerBuilder extends Component {
       deliveryMethod: 'fastest'
     }
     /* for firebase the post request goes any "node" (/orders) + .json
-    appended to baseURL */
+    - appended to baseURL
+    - comment out 108-114 to see the loading animation after pressing continue */
     axios.post('/orders.json', order)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
+      .then(response => {
+        this.setState( {loading: false, purchasing: false});
+      })
+      .catch(error => {
+        this.setState( {loading: false, purchasing: false});
+      });
   }
 
   render () {
@@ -113,17 +121,23 @@ class BurgerBuilder extends Component {
     for (let key in disabledInfo) {
       disabledInfo[key] = disabledInfo[key] <= 0;
     }
+    let orderSummary = <OrderSummary
+      ingredients={this.state.ingredients}
+      price={this.state.totalPrice}
+      purchaseCancelled={this.purchaseCancelHandler}
+      purchaseContinued={this.purchaseContinueHandler} />;
+
+    if (this.state.loading) {
+      orderSummary = <Spinner />;
+    }
+
     // {salad: true, bacon: true, cheese: false, meat: false}
     return (
       <Aux>
         <Modal
           show={this.state.purchasing}
-          modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler} />
+          modalClosed={this.purchaseCancelHandler} >
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
